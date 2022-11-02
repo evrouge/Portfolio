0d75f38d6185959910efc2afb1f7d1e612d8c80e
Evin Lerouge <evinlerouge@Evins-MacBook-Air.local>
2022-11-02 09:03:41 -0600

    starting buttons

---
+++ app.js
@@ -0,0 +1,14 @@
+//Creating a web page portfolio with jQuery/JS
+
+
+
+$(() => {
+    //modal (pop-up) button for bio
+
+
+    //button for resume
+    $('#middle-button').on('click',)
+
+    //button for resume
+    //$('#bottom-button').on('click', )
+})
