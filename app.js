8b366d96b904d4334565e7719431f69289159350
Evin Lerouge <evinlerouge@Evins-MacBook-Air.local>
2022-11-03 10:21:20 -0600

    mouse event working

--- app.js
+++ app.js
@@ -2,15 +2,35 @@
 
 
 $(() => {
-    //omg how does this ACTUALLY work
-    //accordian expand / collapse for resume
+    // gonna try making a modal happen here
+
+
+
+    // omg how does this ACTUALLY work
+    // accordion expand / collapse for resume
     $('.expand').click(function () {
-        $('.collapse').slideToggle(1000);
+        $('.collapse').slideToggle(700);
     })
 
-    //expand and collapse for portfolio
+    // accordion expand / collapse for portfolio
     $('.expand2').click(function () {
-        $('.collapse2').slideToggle(1000);
+        $('.collapse2').slideToggle(700);
+    })
+
+    // mouseover mouseleave function for linkedin works!!
+    $('#hover-linkedin').mouseover(function () {
+        $('#hover-linkedin').css('background', 'green');
+    })
+    $('#hover-linkedin').mouseleave(function () {
+        $('#hover-linkedin').css('background-color', 'pink');
+    })
+
+    //mouseover mouseleave for github
+    $('#hover-github').mouseover(function () {
+        $('#hover-github').css('background-color', 'blue');
+    })
+    $('#hover-github').mouseout(function () {
+        $('#hover-github').css('background-color', 'pink');
     })
 
 })
